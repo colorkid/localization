77c5b49a09ec953f72f35acd7b3eae4488274eb7
Musatov Yuriy <colorkid@yandex.ru>
2019-09-20 09:16:01 +0300

    Update test.js

--- test.js
+++ test.js
@@ -4,7 +4,7 @@
 val = 1; // var varName = 1, но лучше использовать let или const, что-бы предотвратить засорение глобального объекта
 // assign numbers to each button
 var buttons = document.getElementsByTagName("button");
-for (var i = 0; i < buttons.length; i++) { // лучше не использовать объявление переенной с ключевым словом var
+for (var i = 0; i < buttons.length; i++) { // лучше не использовать объявление переменной  с ключевым словом var
   buttons[i].addEventListener("click", function() {
     // each button should have its value.
     alert("You pressed button " + i); // к моменту вызова анонимной функции при события клик, i будет уже равно buttons.length.
@@ -12,22 +12,23 @@ for (var i = 0; i < buttons.length; i++) { // лучше не использов
 }
 
 try {
-   const res = await askBackend(); // await работает только внутри async–функций
-   if (res.status = ‘Success’) // = нужен оператор сравнения а не присвоения, а также не корректные скобки
-     buttons[1].innerHtml = res.text; // корректно innerHTML, но так как это кнопка то лучше использовать textContent
+  const res = await askBackend(); // await работает только внутри async–функций
+  if (res.status = ‘Success’) // = нужен оператор сравнения а не присвоения, а также не корректные скобки
+    buttons[1].innerHtml = res.text; // корректно innerHTML, но так как это кнопка то лучше использовать textContent
 }
-catch (err) { // ошибка в const res = await askBackend(); здесь не словится
+catch (err) { // ошибка в const res = await askBackend(); здесь не cловим
   alert(‘Something went wrong: ‘ + err + ‘backend response: ’ + res); // res - находиться в другой области видимости, не коретная строка
 }
 
+/*------------------------------------------------------------------*/
 
 // 2. - Выполнится ли программа ниже? Если да, то как будет выглядеть массив `arr`?
-var arr = [1, 1]; // объевляем массив [1, 1]
-arr[2] = 1; // добавляем в масив по индексу 2 знаяение три 1
-console.log(arr[6] = undefined); // добавляем в массив по индексу 6 знаяение три undefined, создаются ячейки массива с индекса 2 до 6
-
+var arr = [1, 1]; // объявляем  массив [1, 1]
+arr[2] = 1; // добавляем в маcсив по индексу 2 значение три - 1
+console.log(arr[6] = undefined); // добавляем в массив по индексу 6 значение три undefined, создаются ячейки массива с индекса 2 до 6
 // Программа выполнится, так как в ней нет ошибок хоть и строчка №3 не имеет смысла, но она валидна. Массив arr будет иметь вид: [1, 1, 1, undefined, undefined, undefined, undefined]
 
+/*------------------------------------------------------------------*/
 
 // 3. - Что выведет в консоль код ниже? Используя средства ES6, как добиться предполагаемого результата?
 // Решение на ES6 https://jsfiddle.net/mozt36n7/1/
@@ -40,6 +41,7 @@ function f() {
 (new f)();
 // в консоле будет undefined, так как данная запись будет интерпретирована как вызов функции с window в качестве this.
 
+/*------------------------------------------------------------------*/
 
 // 4. - Please write a function wrapper slow_guard(timeout, fn) that logs a warning if
 // the function takes too long to complete. Here is a typical example of how your wrapper can be used:
@@ -57,8 +59,9 @@ function slow_guard(timeout, fn) {
 		return result;
   };
 }
-
 // Демо: https://jsfiddle.net/h985spjo/4/
+
+/*------------------------------------------------------------------*/
 
 // REACT
 // 1. - Code simple (named Simple) React component (preferably on ES6) that consists of two parts: 
@@ -68,8 +71,12 @@ function slow_guard(timeout, fn) {
 
 // Решение - https://jsfiddle.net/Lmy8jhxc/
 
+/*------------------------------------------------------------------*/
 
-// 2. -Imagine that you are designing a new huge website with React. How would you implement localization? Give simple snippets for a button and a list element (of your choice). A list contains languages (ENG, RUS, CH, JAP), button confirms the selection and changes its language. Language is saved after reload, default - ENG.
+// 2. -Imagine that you are designing a new huge website with React. How would you implement localization?
+// Give simple snippets for a button and a list element (of your choice). A list contains languages (ENG, RUS, CH, JAP),
+// button confirms the selection and changes its language. Language is saved after reload, default - ENG.
+
 import React, { Component } from 'react';
 import LocalizedStrings from 'react-localization';
 
@@ -139,6 +146,8 @@ export default App;
 
 // Демо - https://colorkid.github.io/localization/
 
+/*------------------------------------------------------------------*/
 
-// 3. - Assume that you are working on a relatively big project. It is React SPA with a lot of dialogs. You were asked to add Google Analytics on pages and track different actions. What questions would you ask? 
+// 3. - Assume that you are working on a relatively big project.
+// It is React SPA with a lot of dialogs. You were asked to add Google Analytics on pages and track different actions. What questions would you ask? 
 // Попрошу в письменной форме подробно какие действия страницы и условия нужно отслеживать и по каким условиям.
